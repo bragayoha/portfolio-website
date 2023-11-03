cbc92cd18010bf271c3a933f99edb0818a70821b
Yoha Braga <braga.yoha@gmail.com>
2023-11-03 17:47:12 -0300

    feat: animated

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,12 +2,18 @@
 import React from 'react'
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion'
 
 const HeroSection = () => {
     return (
         <section className='lg:py-16'>
             <div className='grid grid-cols-1 sm:grid-cols-12'>
-                <div className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
+                <motion.div 
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className='col-span-8 place-self-center text-center sm:text-left justify-self-start'
+                >
                     <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold'>
                         <span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-800 to-secondary-800'>
                             Olá, eu sou o {''} 
@@ -17,9 +23,9 @@ const HeroSection = () => {
                         sequence={[
                             'Yoha',
                             1000,
-                            'Desenvolvedor Backend',
+                            'Dev Backend',
                             1000,
-                            'Desenvolvedor Web',
+                            'Dev Web',
                             1000
                         ]}
                         wrapper="span"
@@ -40,8 +46,13 @@ const HeroSection = () => {
                             </span>
                         </button>
                     </div>
-                </div>
-                <div className='col-span-4 place-self-center mt-4 lg:mt-0'>
+                </motion.div>
+                <motion.div 
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className='col-span-4 place-self-center mt-4 lg:mt-0'
+                >
                     <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
                         <Image
                             src='/images/hero-img.png'
@@ -51,7 +62,7 @@ const HeroSection = () => {
                             height={300}
                         />
                     </div>
-                </div>
+                </motion.div>
             </div>
         </section>
     )
