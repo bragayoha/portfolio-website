e156085521b10a904edd47cd2ffaa6c401ecd6d9
Yoha Braga <braga.yoha@gmail.com>
2023-11-03 16:42:53 -0300

    feat: achievements section

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='footer border-t border-[#33353F] text-white'>
+    <footer className='footer border-t z-10 border-[#33353F] text-white'>
         <div className='container p-12 flex justify-between'>
             <span>LOGO</span>
             <p className='text-slate-800'>All rights reserved.</p>
