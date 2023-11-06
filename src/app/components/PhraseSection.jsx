fe21f573af2a30ed9c266ae9d958706843c46b25
Yoha Braga <braga.yoha@gmail.com>
2023-11-06 19:53:59 -0300

    fix: deploy error

--- src/app/components/PhraseSection.jsx
+++ src/app/components/PhraseSection.jsx
@@ -6,7 +6,7 @@ const PhraseSection = () => {
     <div className='py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
       <div className='sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex items-center justify-center'>
         <p className='text-[#ADB7BE] font-semibold text-base sm:text-lg mb-6 lg:text-xl flex justify-center items-center'>
-          "A arte de programar consiste em organizar e dominar a complexidade."
+          A arte de programar consiste em organizar e dominar a complexidade.
           {' '} 
           - Edsger W. Dijkstra
         </p>
