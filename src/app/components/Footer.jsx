901d329761cb5dd5b98800ba44b0be9fc4ceab41
Yoha Braga <braga.yoha@gmail.com>
2023-11-06 19:31:13 -0300

    fix: logo and 'lorem ipsum'

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Pixelify_Sans } from 'next/font/google'
+
+const logoFontFamily = Pixelify_Sans({
+    weight: ['400', '500', '600', '700'],
+    subsets: ['latin']
+})
 
 const Footer = () => {
   return (
-    <footer className='footer border-t z-10 border-[#33353F] text-white'>
-        <div className='container p-12 flex justify-between'>
-            <span>
-              Logo
+    <footer className='footer border-t z-10 border-[#33353F] text-[#D4C1A9]'>
+        <div className='container p-10 flex justify-between'>
+            <span className={logoFontFamily.className}>
+              YOHA
             </span>
             <p className='text-slate-800'>All rights reserved.</p>
         </div>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -35,7 +35,7 @@ const HeroSection = () => {
                         />
                     </h1>
                     <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+                        Desenvolvendo soluções desde 2014.
                     </p>
                     <div>
                         <Link href='#contact'>
